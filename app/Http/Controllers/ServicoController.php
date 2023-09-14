e8ddf4610fe6121363a44ada7028d0d3837c60ca
biatozeti <anabeatriztozetidasilva@gmail.com>
2023-09-14 15:29:30 -0300

    atualizando

---
+++ app/Http/Controllers/ServicoController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServicoFormRequest;
+use App\Models\servico;
+use Illuminate\Http\Request;
+
+class ServicoController extends Controller
+
+    {
+        public function store(ServicoFormRequest $request)
+        {
+          $usuario = servico::create([
+      
+            'nome' => $request->nome,
+            'descricao' => $request->descricao,
+            'duracao' => $request->duracao,
+            'preco' => $request->preco,
+          ]);
+          return response()->json([
+            "success" => true,
+            "message" => "usuario Cadastrado com sucesso",
+            "data" => $usuario
+          ], 200);
+        }
+
+        public function pesquisarPorNome(Request $request)
+        {
+          $usuario = servico::where('nome', 'like', '%' . $request->nome . '%')->get();
+      
+          if (count($usuario) > 0) {
+            return response()->json([
+              'status' => true,
+              'data' => $usuario
+            ]);
+          }
+      
+          return response()->json([
+            'status' => false,
+            'message' => 'nao ha resultados para pesquisa'
+          ]);
+        }
+
+        public function pesquisarPorDescricao(Request $request)
+        {
+          $usuario = servico::where('descricao', 'like', '%' . $request->descricao. '%')->get();
+      
+          if (count($usuario) > 0) {
+            return response()->json([
+              'status' => true,
+              'data' => $usuario
+            ]);
+          }
+      
+          return response()->json([
+            'status' => false,
+            'message' => 'nao ha resultados para pesquisa'
+          ]);
+        }
+        public function update(request $request)
+        {
+          $usuario = servico::find($request->id);
+      
+          if (!isset($usuario)) {
+            return response()->json([
+              'status' => false,
+              'message' => "cadastro nao encontrado "
+            ]);
+          }
+          if (isset($request->nome)) {
+            $usuario->nome = $request->nome;
+          }
+      
+          if (isset($request->descricao)) {
+            $usuario->descricao = $request->descricao;
+          }
+          if (isset($request->duracao)) {
+            $usuario->duracao= $request->duracao;
+          }
+          if (isset($request->preco)) {
+            $usuario->preco= $request->preco;
+          }
+
+
+      
+          $usuario-> update();
+      
+          return response()->json([
+            'status' => true,
+            'message' => 'cadastro atualizado'
+          ]);
+        }
+        public function excluir($id)
+  {
+    $usuario= servico::find($id);
+
+    if (!isset($usuario)) {
+      return response()->json([
+        'status' => false,
+        'message' => "cadastro nao encontrado "
+      ]);
+    }
+
+    $usuario->delete();
+    return response()->json([
+      'status' => true,
+      'message' => "cadastro excluido com sucesso"
+    ]);
+  }
+
+
+}
---
+++ app/Http/Requests/ServicoFormRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ServicoFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'nome' => 'required|max:80|min:5|unique:servicos,nome',
+            'descricao' => 'required|max:200|min:10|',
+            'duracao' => 'required|numeric',
+            'preco' => 'required|decimal:2',
+            
+            
+        ];
+    }
+        public function failedValidation(Validator $validator){
+            throw new HttpResponseException(response()->json([
+                'success' => false,
+                'error' => $validator->errors()
+            ]));
+        }
+            public function messages()
+            {
+                return[
+                    'nome.required' =>'o campo nome é obrigatorio',
+                    'nome.max' => 'o campo nome deve conter no maximo 80 caracteres',
+                    'nome.min' => 'o campo nome deve conter no minimo 5 caracteres',
+                    'nome.unique' => 'nome ja cadastrado no sistema',
+                    'descricao.required' => 'descricao obrigatorio',
+                    'descricao.max' => 'a descricao deve conter no maximo 200 caracteres',
+                    'descricao.min' => 'a descricao deve conter no minimo 10 caracteres',
+                    'duracao.required' => 'duracao obrigatorio',
+                    'duracao.numeric' => 'formato invalido, so aceito number',
+                    'preco.decimal' => 'formato invalido',
+                    'preco.required' => 'preco obrigatorio',
+                    
+                ];
+                    
+    }
+    }
